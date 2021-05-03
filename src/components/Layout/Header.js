876829a84cef9b5975272ca674df6177595eee72
Andrew Pham <andrewpham92@gmail.com>
2021-05-03 13:06:11 -0400

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './Header.module.css'
+import mealsImage from '../../assets/meals.jpeg'
+import HeaderCardButton from './HeaderCardButton'
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food'/>
+      </div>
+    </>
+  )
+}
+
+export default Header
