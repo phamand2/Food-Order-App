5945a5830bc2aa0954859cacce00067b8db336b1
Andrew Pham <andrewpham92@gmail.com>
2021-05-05 12:12:12 -0400

    added animation to the header cart

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,19 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import cartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCardButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighlighted] = useState(false)
 
   const cartCtx = useContext(cartContext)
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const {items} = cartCtx
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount
   }, 0)
 
+  const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0){
+      return
+    }
+    setBtnIsHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }
+    
+  }, [items])
+
 
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
