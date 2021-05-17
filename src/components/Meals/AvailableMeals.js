920a75cec6dd04170f069fe5011643049495cc28
Andrew Pham <andrewpham92@gmail.com>
2021-05-17 15:38:56 -0400

    added error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,31 +9,46 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading]= useState(true)
+  const [httpError,setHttpError] = useState()
 
   useEffect(()=> {
     const fetchMeals = async()=> {
-      const response = await fetch('https://food-order-2ce5b-default-rtdb.firebaseio.com/meals.json')
-      const responseData = await response.json()
-
-      const loadedMeals = []
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price
-        })
-        }
-
-        setMeals(loadedMeals)
-        setIsLoading(false)
+      
+        const response = await fetch('https://food-order-2ce5b-default-rtdb.firebaseio.com/meals.json')
+        const responseData = await response.json()
+  
+        const loadedMeals = []
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price
+          })
+          }
+  
+          setMeals(loadedMeals)
+          setIsLoading(false)
+    
       }
-      fetchMeals()
+
+        fetchMeals().catch(error => {
+          setIsLoading(false)
+          setHttpError(error.message)
+        })
+
+
     },[])
 
     if(isLoading){
       return (<section className={classes.mealsLoading}>
         <p>Loading... </p>
+      </section>)
+    }
+
+    if(httpError){
+      return (<section className={classes.mealsError}>
+        <p>{httpError} </p>
       </section>)
     }
 
